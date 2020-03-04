7e1e157a95bd3ea7129176f6a59c183f0c7a3c8a
kkulma <kkulma@mango-solutions.com>
2020-03-04 12:36:55 +0000

    added get_current_mix()

--- R/get_api.R
+++ R/get_api.R
@@ -2,6 +2,7 @@
 #'
 #' @param start {character} A start date of the intesity data
 #' @param end {character} An end date of the intesity data
+#' @param regional {logical} Whether the carbon intensity data should contain regional break down. Set to FALSE by default.
 #'
 #' @return a data.frame with 1/2-hourly carbon intensity data for specified time period
 #' @export
@@ -10,20 +11,67 @@
 #' start <- "2019-04-01"
 #' end <- "2019-04-07"
 #' get_intensity(start, end)
+#' get_intensity(start, end, regional = TRUE)
 #' }
-get_intensity <- function(start, end) {
-  url <- "https://api.carbonintensity.org.uk/intensity/"
+get_intensity <- function(start, end, regional = FALSE) {
+
+  if (regional) {
+    url <- url <- 'https://api.carbonintensity.org.uk/regional/intensity/'
+  } else {
+    url <- "https://api.carbonintensity.org.uk/intensity/"
+  }
+
   from_date <- paste0(as.Date(start), "T00:00Z/")
   to_date <- paste0(as.Date(end), "T23:59Z")
 
   call <- paste0(url, from_date, to_date)
 
   get_data <- httr::GET(call)
-  get_data_content <- httr::content(get_data, as = "text")
 
-  data <- jsonlite::fromJSON(get_data_content, flatten = TRUE)[[1]]
-  clean_names <- gsub('intensity.', '', colnames(data))
-  colnames(data) <- clean_names
 
+  if (get_data$status_code != 200) {
+    stop(paste0("ERROR: The status call is ", get_data$status_code))
+  } else {
+    get_data_content <- httr::content(get_data, as = "text", encoding = "UTF-8")
+
+    data <- jsonlite::fromJSON(get_data_content, flatten = TRUE)[[1]]
+
+    if (regional) {
+      data <- data %>%
+        tidyr::unnest(rlang::sym("regions")) %>%
+        tidyr::unnest(rlang::sym("generationmix"))
+    }
+
+    clean_names <- gsub('intensity.', '', colnames(data))
+    colnames(data) <- clean_names
+  }
   data
+}
+
+
+#' Get generation mix for current half hour
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples \dontrun{
+#' get_current_mix()}
+get_current_mix <- function() {
+  url <- 'https://api.carbonintensity.org.uk/generation'
+  get_data <- httr::GET(url)
+
+  if (get_data$status_code != 200) {
+    stop(paste0("ERROR: The status call is ", get_data$status_code))
+  } else {
+    get_data_content <- httr::content(get_data, as = "text", encoding = "UTF-8")
+
+    data <- jsonlite::fromJSON(get_data_content, flatten = TRUE)[[1]]
+
+    result <- data$generationmix %>%
+      dplyr::mutate(from = lubridate::ymd_hm(data$from),
+             to = lubridate::ymd_hm(data$to))
+  }
+
+ result
+
 }
