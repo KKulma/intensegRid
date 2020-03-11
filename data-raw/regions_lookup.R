2293922d5740c02579b906df9090a041ff3ad763
kkulma <kkulma@mango-solutions.com>
2020-03-11 09:10:03 +0000

    organised data files

--- data-raw/regions_lookup.R
+++ data-raw/regions_lookup.R
@@ -1,14 +1,5 @@
-#' @title regions_lookup
-#' @description A lookup table of region_ids and corresponding GB regions
-#' @format A data frame with 17 rows and 2 variables:
-#' \describe{
-#'   \item{\code{Region ID}}{region ID to be used in intensegRid package}
-#'   \item{\code{Shortname}}{corresponding GB region}
-#'}
-#' @source \url{https://carbon-intensity.github.io/api-definitions/#region-list}
-"regions_lookup"
-
 library(rvest)
+library(dplyr)
 
 url <- "https://carbon-intensity.github.io/api-definitions/?python#region-list"
 
