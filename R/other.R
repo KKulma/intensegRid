68f8a8aaa98aa05a236e13f144bcb74977e22a9e
kkulma <kkulma@mango-solutions.com>
2020-05-12 22:37:06 +0100

    added error handling to other.R funcs

--- R/other.R
+++ R/other.R
@@ -16,8 +16,8 @@ get_factors <- function() {
 
 #' Get Carbon Intensity statistics between from and to dates
 #'
-#' @param start {character} A start date of the stats data
-#' @param end {character} An end date of the stats data
+#' @param start {character} A start date of the stats data. The maximum date range is limited to 30 days.
+#' @param end {character} An end date of the stats data. The maximum date range is limited to 30 days.
 #' @param block {numeric} Block length in hours i.e. a block length of 2 hrs over a 24 hr period returns 12 items with the average, max, min for each 2 hr block
 #'
 #' @return tibble
@@ -25,13 +25,29 @@ get_factors <- function() {
 #'
 #' @examples \dontrun{
 #' start <- "2019-04-01"
-#' end <- "2019-04-07"
+#' end <- "2019-05-01"
 #' get_stats(start, end)
 #' get_stats(start, end, block = 2)
 #' }
 #'
 get_stats <- function(start, end, block = NULL) {
   url <- "https://api.carbonintensity.org.uk/intensity/stats/"
+  
+  if(as.numeric(as.Date(end) - as.Date(start)) > 30) {
+    stop(paste0("The start & end date range should not exceed 30 days. Currently it is ",
+                as.numeric(as.Date(end) - as.Date(start)),
+                " days"))
+  }
+  
+  if(!is.null(block) && block > 24) {
+    stop("Block value should not exceed 24")
+  }
+  
+  if(!is.null(block) && block < 1) {
+    stop("Block value should not be lower than 1")
+  }
+  
+  
   
   from_date <- paste0(as.Date(start), "T00:00Z/")
   to_date <- paste0(as.Date(end), "T23:59Z")
