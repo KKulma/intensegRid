703724df0e0b660507d6d507a7c16dbbf0e25f4b
kkulma <katarzyna.kulma@gmail.com>
2020-11-07 19:52:26 +0000

    tidied up R/ files

--- R/generationmix.R
+++ R/generationmix.R
@@ -13,32 +13,31 @@
 #' get_mix()}
 get_mix <- function(start = NULL, end = NULL) {
   url <- 'https://api.carbonintensity.org.uk/generation/'
-
+  
   if (all(!is.null(c(start, end)))) {
     from_date <- paste0(as.Date(start), "T00:00Z/")
     to_date <- paste0(as.Date(end), "T23:59Z")
-
+    
     call <- paste0(url, from_date, to_date)
   } else {
     call <- url
   }
-
+  
   data <- get_data(call)
-
+  
   if (all(!is.null(c(start, end)))) {
     result <- data %>%
-      tidyr::unnest(.data$generationmix) %>% 
-      dplyr::mutate(
-        from = lubridate::ymd_hm(.data$from),
-        to = lubridate::ymd_hm(.data$to)) %>%
-          tibble::as_tibble()
+      tidyr::unnest(.data$generationmix) %>%
+      dplyr::mutate(from = lubridate::ymd_hm(.data$from),
+                    to = lubridate::ymd_hm(.data$to)) %>%
+      tibble::as_tibble()
   } else {
     result <- data$generationmix %>%
       dplyr::mutate(from = lubridate::ymd_hm(data$from),
                     to = lubridate::ymd_hm(data$to)) %>%
       tibble::as_tibble()
   }
-
+  
   result
-
+  
 }
--- R/utils.R
+++ R/utils.R
@@ -4,22 +4,21 @@
 #'
 #' @return tibble
 get_data <- function(call) {
-
   response <- httr::GET(call)
-
+  
   if (httr::http_type(response) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-
+  
   if (response$status_code != 200) {
     stop(paste0("ERROR: The status call is ", response$status_code))
   }
-
+  
   response_content <-
     httr::content(response, as = "text", encoding = "UTF-8")
-
+  
   data <-
     jsonlite::fromJSON(response_content, flatten = TRUE)[[1]]
-
+  
   data
 }
